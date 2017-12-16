f93078436947d980452d13df51d90ab6cef7db8f
Adam <alspdx@gmail.com>
2017-12-15 17:11:30 -0800

    add animation to minimize hero when selecting a category and display again when selecting home

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,24 @@
-
-
+// Attach click to dropdown menu items
 function attachClick() {
   $('.dropdown').off('click', 'li').on('click', 'li', function() {
+    $('.hero').css('height', '0');
+    $('.gallery').css('margin-top', '70px');
     showOnly($(this).text());
-  })
+  });
 }
 
+// Only show results from clicked dropdown menu category
 function showOnly(category) {
-  var selection = '.' + category.toLowerCase();
-  $('.gallery-image').css('display', 'none');
-  $(selection).css('display', 'block');
+  if (category === 'Home') {
+    $('.hero').css('height', '100vh');
+    $('.gallery-image').css('display', 'block');
+  } else {
+    var selection = '.' + category.toLowerCase();
+    $('.gallery-image').css('display', 'none');
+    $(selection).css('display', 'block');
+  }
 }
 
 $(function() {
   attachClick();
-
-
 });
